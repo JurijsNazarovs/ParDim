1354f68859e35a9052e4f3a820e4c13962fc31d9
Jurijs Nazarovs <nazarovs@wisc.edu>
2017-10-12 02:24:24 -0500

    now file/combination creates as temporary file and then copy in otherdirs. Also, fixed bug about cleaning directorues if something is wrong.

--- scripts/exeDownload.sh
+++ scripts/exeDownload.sh
@@ -7,13 +7,23 @@
 shopt -s nullglob #allows create an empty array
 shopt -s extglob #to use !
 
+CleanOutput() {
+  local dirTmp="$1"
+  local outTar="$2"
+  
+  mv !("$dirTmp") "$dirTmp"
+  mv "$dirTmp"/_condor_std* ./
+  mv "$dirTmp/$outTar" ./
+}
+
+
 ## Input and default values
 link=${1}
 path=${2:-./}
 delim=${3:-,} #use to split path
 delimJoin=${4:-;} #use to split link
 outTar=${5} #tarFile to return back on submit machine
-isDry=${6:-true}
+isDry=${6:-false}
 
 
 ## Initial preparation
@@ -23,7 +33,7 @@ readarray -t link <<< "$(echo "$link" | tr "$delimJoin" "\n")"
 dirTmp=$(mktemp -dq tmpXXXX) #create tmp folder to tar everything inside later
 
 
-## Dowloading file
+## Downloading file
 wget "${link[@]}"
 exFl=$?
 if [ "$exFl" -ne 0 ]; then
@@ -32,42 +42,52 @@ if [ "$exFl" -ne 0 ]; then
 fi
 
 
+## Create single or joined temporary file
+fileTmp=$(mktemp -uq downloadedFileTmpXXXX) #create tmp file to join other files
+if [[ ${#link[@]} -eq 1 ]]; then
+    mv ${link##*/} "$fileTmp"
+else
+  cat "${link[@]##*/}" > "$fileTmp" #join several files in one
+  exFl=$? #exit value of coping
+  if [[ "$exFl" -ne 0 ]]; then
+      echo "Joining files was not successful! Error code: $exFl"
+      CleanOutput "$dirTmp" "$outTar"
+      exit $exFl
+  fi
+  rm -rf "${link[@]##*/}"
+fi
+ 
+
 ## Copy file in a right directory
 for filePath in "${path[@]}"; do
   curDir="${filePath%/*}"
-  mkdir -p "$curDir" #directory for downlaoded file
-  
-  if [[ ${#link[@]} -eq 1 ]]; then
-      cp ${link##*/} "$filePath"
-  else
-    cat "${link[@]##*/}" > "${filePath##*/}" #join several files in one
-    exFl=$? #exit value of coping
-    if [[ "$exFl" -ne 0 ]]; then
-        echo "Joining files was not successful! Error code: $exFl"
-        exit $exFl
-    fi
-    cp "${filePath##*/}" "$filePath"
-  fi
+  mkdir -p "$curDir" #directory for downloaded file
+  cp "$fileTmp" "$filePath"
   
   exFl=$? #exit value of coping
   if [[ "$exFl" -ne 0 ]]; then
       echo "Coping was not successful! Error code: $exFl"
+      CleanOutput "$dirTmp" "$outTar"
       exit $exFl
   else
     echo "Success! Check file: $filePath"
-    rm -rf "${link[@]}"
     mv "$curDir" "$dirTmp"
   fi
 done
 
-## Prepare tar to move results back
-tar -czf "$outTar" -C "$dirTmp" .
 
-# Has to hide all unnecessary files in tmp directories 
+## Prepare tar to move results back
+env GZIP=-9 tar -czf "$outTar" -C "$dirTmp" . #to compress with max level
+exFl=$?
+if [[ "$exFl" -ne 0 ]]; then
+    echo "Creating tar was not successful! Error code: $exFl"
+    CleanOutput "$dirTmp" "$outTar"
+    exit $exFl
+fi
+
 if [[ "$isDry" = false ]]; then
-    mv !("$dirTmp") "$dirTmp"
-    mv "$dirTmp"/_condor_std* ./
-    mv "$dirTmp/$outTar" ./
+    echo "Final step: moving files in $outTar"
+    CleanOutput "$dirTmp" "$outTar"
 fi
 
 exit 0
